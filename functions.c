e0afceee26365a581261404027c0f39f71eed116
Matthew Goodship <matthewwgoodship@icloud.com>
2021-04-04 22:32:29 +0100

    Chapter 8 Arguments

--- functions.c
+++ functions.c
@@ -22,3 +22,12 @@ int power(int base, int n) {
         p = p * base;
     return p;
 }
+
+/* power: raise base to the n-th power; n >= 0; version 2 */
+int power(int base, int n) {
+    int p;
+    
+    for (p = 1; n > 0; --n)
+        p = p * base;
+    return p;
+}
